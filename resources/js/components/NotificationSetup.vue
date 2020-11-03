0fa9afc1494020305dd8b96568574eac86d51c8e
Mike Jenkins <mjenkins.jw@gmail.com>
2020-11-03 14:13:26 +0100

    Notifications UI

--- resources/js/components/NotificationSetup.vue
+++ resources/js/components/NotificationSetup.vue
@@ -1,157 +1,141 @@
 <template>
-  <v-card class="mx-auto" max-width="600" outlined>
-    <v-card-title>
-      Notifications Setup
-    </v-card-title>
+<v-card class="mx-auto" max-width="600" outlined>
+  <v-card-title>
+    {{ $t('notifications.notification_setup') }}
+  </v-card-title>
 
-    <v-card-text>
-      <v-stepper v-model="stepper">
-        <v-stepper-items>
-          <v-stepper-content step="1">
-            <v-row>
-              <v-col cols=12 class="my-5 text-center">
-                <h3 class="ma-3">Let's setup notifications for your team</h3>
-                <p class="text-muted my-8">
-                  Enabling this feature ensures your team is notified about trade offers, new schedules, etc.
-                </p>
-                <p class="text-muted my-8">
-                  SheepStand uses Telegram for notifications.
-                  Telegram is a free and secure messaging app, similar to Viber and WhatsApp.
-                  If you do not already have a Telegram account, you can download the app from the link below and sign up.
-                </p>
-                <p class="my-8">
-                  <TelegramDL />
-                </p>
-                <p class="text-muted my-8">
-                  After signing up for an account, or if you are already a Telegram user, continue to the next step.
-                </p>
-                        
-                <v-btn color="primary" @click="stepperGo(2)">I have a Telegram account</v-btn>
-              </v-col>
-            </v-row>
-          </v-stepper-content>
+  <v-card-text>
+    <v-stepper v-model="stepper">
+      <v-stepper-items>
+        <v-stepper-content step="1">
+          <v-row>
+            <v-col cols=12 class="my-5 text-center">
+              <h3 class="ma-3">{{ $t('notifications.lets_setup_notifications') }}</h3>
+              <p class="text-muted my-8">
+                {{ $t('notifications.feature_explanation') }}
+              </p>
+              <p class="text-muted my-8">
+                {{ $t('notifications.sheepstand_uses_telegram') }}
+                {{ $t('notifications.if_no_telegram_account') }}
+              </p>
+              <p class="my-8">
+                <TelegramDL />
+              </p>
+              <p class="text-muted my-8">
+                {{ $t('notifications.after_telegram_signup') }}
+              </p>
 
+              <v-btn color="primary" @click="stepperGo(2)">{{ $t('notifications.i_have_telegram') }}</v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
 
+        <v-stepper-content step="2">
+          <v-row>
+            <v-col cols=12 class="my-5 text-center">
+              <h3 class="ma-3">{{ $t('notifications.signin_to_telegram') }}</h3>
+              <p class="text-muted my-8">
+                {{ $t('notifications.enter_telegram_phone_number') }}
+              </p>
 
-          <v-stepper-content step="2">
-            <v-row>
-              <v-col cols=12 class="my-5 text-center">
-                <h3 class="ma-3">Sign In to Telegram</h3>
-                <p class="text-muted my-8">
-                  Please enter the phone number linked to your Telegram account. 
-                  A code will be sent to this number in order to sign in.
-                </p>
+              <div class="my-16">
+                <MazPhoneNumberInput v-model="phone_num" show-code-on-list size="lg" :dark="$vuetify.theme.dark" fetch-country />
+              </div>
 
-                <div class="my-16">
-                  <MazPhoneNumberInput v-model="phone_num" show-code-on-list size="lg" :dark="$vuetify.theme.dark" fetch-country/>
-                </div>
+              <p class="text-muted mb-8 mt-n12 red--text" v-if="error_msg">
+                {{ $t('general.error') + ': ' + error_msg }}
+              </p>
 
-                <p class="text-muted mb-8 mt-n12 red--text" v-if="error_msg">
-                  {{ $t('general.error') + ': ' + error_msg }}
-                </p>
+              <v-btn text @click="cancelSetup">{{ $t('general.cancel') }}</v-btn>
+              <v-btn color="primary" @click="sendCode()">{{ $t('general.continue') }}</v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
 
-                <v-btn text @click="cancelSetup">Cancel</v-btn>
-                <v-btn color="primary" @click="sendCode()">Continue</v-btn>
-              </v-col>
-            </v-row>
-          </v-stepper-content>
+        <v-stepper-content step="3">
+          <v-row>
+            <v-col cols=12 class="my-5 text-center">
+              <h3 class="ma-3">{{ $t('notifications.enter_login_code') }}</h3>
+              <p class="text-muted my-8">
+                {{ $t('notifications.get_telegram_login_code') }}
+              </p>
 
+              <div class="my-16 text-center">
+                <CodeInput v-model="login_code" :fields="5" @complete="codeInputComplete" class="mx-auto" />
+              </div>
 
+              <p class="text-muted mb-8 mt-n12 red--text" v-if="error_msg">
+                {{ $t('general.error') + ': ' + error_msg }}
+              </p>
 
-          <v-stepper-content step="3">
-            <v-row>
-              <v-col cols=12 class="my-5 text-center">
-                <h3 class="ma-3">Enter the Login Code</h3>
-                <p class="text-muted my-8">
-                  Open Telegram on your device and find the login code you were sent. Enter the code below.
-                </p>
+              <v-btn text @click="cancelSetup">{{ $t('general.cancel') }}</v-btn>
+              <v-btn color="primary" @click="signIn()">{{ $t('general.continue') }}</v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
 
-                <div class="my-16 text-center">
-                  <CodeInput v-model="login_code" :fields="5" @complete="codeInputComplete" class="mx-auto" />
-                </div>
+        <v-stepper-content step="4">
+          <v-row>
+            <v-col cols=12 class="my-5 text-center">
+              <h3 class="ma-3">{{ $t('notifications.enter_telegram_password') }}</h3>
+              <p class="text-muted my-8">
+                {{ $t('notifications.2FA_explanation') }}
+              </p>
 
-                <p class="text-muted mb-8 mt-n12 red--text" v-if="error_msg">
-                  {{ $t('general.error') + ': ' + error_msg }}
-                </p>
+              <div class="my-16 text-center">
+                <v-text-field v-model="password" id="txtPassword" class="ma-3" label="2FA Password" outlined :append-icon="showPwd ? 'mdi-eye' : 'mdi-eye-off'" :type="showPwd ? 'text' : 'password'" @click:append="showPwd = !showPwd"></v-text-field>
+              </div>
 
-                <v-btn text @click="cancelSetup">Cancel</v-btn>
-                <v-btn color="primary" @click="signIn()">Continue</v-btn>
-              </v-col>
-            </v-row>
-          </v-stepper-content>
+              <p class="text-muted mb-8 mt-n12 red--text" v-if="error_msg">
+                {{ $t('general.error') + ': ' + error_msg }}
+              </p>
 
+              <v-btn text @click="cancelSetup">{{ $t('general.cancel') }}</v-btn>
+              <v-btn color="primary" @click="check2FA(password)">{{ $t('general.continue') }}</v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
 
+        <v-stepper-content step="5">
+          <v-row>
+            <v-col cols=12 class="my-5 text-center">
+              <h3 class="ma-3">{{ $t('notifications.telegram_signedin') }}</h3>
+              <p class="text-muted my-8">
+                {{ $t('notifications.create_telegram_group') }}
+              </p>
 
-          <v-stepper-content step="4">
-            <v-row>
-              <v-col cols=12 class="my-5 text-center">
-                <h3 class="ma-3">Enter your Telegram Password</h3>
-                <p class="text-muted my-8">
-                  It looks like you enabled 2-Factor Authentication on your account. Please enter you 2FA password to sign in.
-                </p>
+              <div class="my-16 text-center">
+                <v-text-field v-model="group_name" id="txtGroupName" class="ma-3" label="Group Name" outlined></v-text-field>
+                <v-text-field v-model="group_desc" id="txtGroupDesc" class="ma-3" label="Group Description (optional)" outlined></v-text-field>
+              </div>
 
-                <div class="my-16 text-center">
-                  <v-text-field v-model="password" id="txtPassword" class="ma-3" label="2FA Password" outlined
-                    :append-icon="showPwd ? 'mdi-eye' : 'mdi-eye-off'" :type="showPwd ? 'text' : 'password'" @click:append="showPwd = !showPwd"></v-text-field>
-                </div>
+              <p class="text-muted mb-8 mt-n12 red--text" v-if="error_msg">
+                {{ $t('general.error') + ': ' + error_msg }}
+              </p>
 
-                <p class="text-muted mb-8 mt-n12 red--text" v-if="error_msg">
-                  {{ $t('general.error') + ': ' + error_msg }}
-                </p>
+              <v-btn text @click="cancelSetup">{{ $t('general.cancel') }}</v-btn>
+              <v-btn color="primary" @click="createSuperGroup()">{{ $t('general.continue') }}</v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
 
-                <v-btn text @click="cancelSetup">Cancel</v-btn>
-                <v-btn color="primary" @click="check2FA(password)">Continue</v-btn>
-              </v-col>
-            </v-row>
-          </v-stepper-content>
+        <v-stepper-content step="6">
+          <v-row>
+            <v-col cols=12 class="my-5 text-center">
+              <h3 class="ma-3">{{ $t('notifications.setup_complete') }}</h3>
+              <p class="text-muted my-8">
+                {{ $t('notifications.notifications_now_enabled') }}
+              </p>
 
+              <v-btn text @click="cancelSetup">{{ $t('general.close') }}</v-btn>
+            </v-col>
+          </v-row>
+        </v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </v-card-text>
 
-
-          <v-stepper-content step="5">
-            <v-row>
-              <v-col cols=12 class="my-5 text-center">
-                <h3 class="ma-3">Great! You're signed in to Telegram</h3>
-                <p class="text-muted my-8">
-                  Now let's create a Telegram group for your team. 
-                  Team members can then join the group in order to receive notifications.
-                </p>
-
-                <div class="my-16 text-center">
-                  <v-text-field v-model="group_name" id="txtGroupName" class="ma-3" label="Group Name" outlined></v-text-field>
-                  <v-text-field v-model="group_desc" id="txtGroupDesc" class="ma-3" label="Group Description (optional)" outlined></v-text-field>
-                </div>
-
-                <p class="text-muted mb-8 mt-n12 red--text" v-if="error_msg">
-                  {{ $t('general.error') + ': ' + error_msg }}
-                </p>
-
-                <v-btn text @click="cancelSetup">Cancel</v-btn>
-                <v-btn color="primary" @click="createSuperGroup()">Continue</v-btn>
-              </v-col>
-            </v-row>
-          </v-stepper-content>
-
-
-
-          <v-stepper-content step="6">
-            <v-row>
-              <v-col cols=12 class="my-5 text-center">
-                <h3 class="ma-3">Setup Complete!</h3>
-                <p class="text-muted text-body-2 my-8">
-                  Congratulations! Notifications have been enabled on your account. 
-                  You can now access notification settings on the Team Settings page.
-                </p>
-
-                <v-btn text @click="cancelSetup">Close</v-btn>
-              </v-col>
-            </v-row>
-          </v-stepper-content>
-        </v-stepper-items>
-      </v-stepper>
-    </v-card-text>
-
-  </v-card>
-  
+</v-card>
 </template>
 
 <script>
@@ -160,7 +144,9 @@ import helper from '~/mixins/helper'
 import mtproto from '~/mixins/telegram'
 import TelegramDL from '~/components/TelegramDL'
 import CodeInput from "vue-verification-code-input"
-import { MazPhoneNumberInput } from 'maz-ui'
+import {
+  MazPhoneNumberInput
+} from 'maz-ui'
 import 'maz-ui/lib/scss/base.scss'
 import 'maz-ui/lib/scss/maz-input.scss'
 import 'maz-ui/lib/scss/maz-phone-number-input.scss'
@@ -174,13 +160,13 @@ export default {
     CodeInput
   },
 
-  data () {
+  data() {
     return {
       showPwd: false,
     }
   },
 
-  created () {
+  created() {
 
   },
 
@@ -199,11 +185,9 @@ export default {
 </script>
 
 <style>
-
-.react-code-input > input {
+.react-code-input>input {
   color: #0288D1 !important;
   font-weight: bold;
   font-size: 24pt !important;
 }
-
 </style>
--- resources/js/pages/teams/join.vue
+++ resources/js/pages/teams/join.vue
@@ -1,132 +1,156 @@
 <template>
-  <v-container fluid>
-    <v-card class="mx-auto" max-width="600" outlined>
-        <v-stepper v-model="stepperCurr">
-          <v-stepper-items>
+<v-container fluid>
+  <v-card class="mx-auto" max-width="600" outlined>
+    <v-card-title>
+      {{ $t('teams.join_existing_team') }}
+    </v-card-title>
 
-            <!-- STEP 1: JOIN OR CREATE A TEAM -->
-            <v-stepper-content step="1">
-              <v-row class="mt-5">
-                <v-col cols=12 class="text-center">
-                  <h3>{{ $t('teams.join_existing_team') }}</h3>
-                  <blockquote class="blockquote text-muted">
-                    {{ $t('teams.what_is_a_team') }}
-                  </blockquote>
-                </v-col>
-              </v-row>
+    <v-card-text>
+      <v-stepper v-model="stepperCurr">
+        <v-stepper-items>
 
-              <v-row class="mt-2">
-                <v-col cols=12 class="text-center">
-                  <h6 class="mt-6">
-                    {{ $t('teams.enter_team_code') }}
-                  </h6>
-                  <v-text-field v-model="teamid" label="TM-XXXXXXXX" outlined :error="teamNotFound" :error-messages="teamNotFoundMsg"></v-text-field>
-                  <v-btn color="secondary" @click.prevent="findTeam">
-                    {{ $t('teams.find_team') }}
-                  </v-btn>
-                </v-col>
-              </v-row>
+          <!-- STEP 1: JOIN OR CREATE A TEAM -->
+          <v-stepper-content step="1">
+            <v-row class="mt-5">
+              <v-col cols=12 class="text-center">
+                <h3>{{ $t('teams.join_existing_team') }}</h3>
+                <p class="text-muted my-8">
+                  {{ $t('teams.what_is_a_team') }}
+                </p>
+                <p class="text-muted my-8">
+                  {{ $t('teams.enter_team_code') }}
+                </p>
 
-              <v-divider class="my-13"></v-divider>
+                <v-text-field v-model="teamid" class="my-16" label="TM-XXXXXXXX" outlined :error="teamNotFound" :error-messages="teamNotFoundMsg"></v-text-field>
 
-              <v-row class="mb-6">
-                <v-col cols=12 class="text-center">
-                  <h6 class="text-muted">
-                    <a @click.prevent="nextStep(3, $t('teams.confirm_create_new_team'))">
-                      {{ $t('teams.create_new_team') }}
-                    </a>
-                  </h6>
-                </v-col>
-              </v-row>
-            </v-stepper-content>
+                <v-btn color="primary" @click.prevent="findTeam">
+                  {{ $t('teams.find_team') }}
+                </v-btn>
+              </v-col>
+            </v-row>
 
+            <v-divider class="my-4"></v-divider>
 
-            <!-- STEP 2: VERIFY TEAM DETAILS (IF JOINING EXISTING TEAM)-->
-            <v-stepper-content step="2">
-              <v-row>
-                <v-col cols=12 class="mt-5 text-center">
-                  <h3>{{ $t('teams.confirm_team_details') }}</h3>
-                  <h6 class="text-muted mb-5">
-                    {{ $t('teams.confirm_team_message') }}
-                  </h6>
+            <v-row>
+              <v-col cols=12 class="text-center">
+                <p class="text-muted my-8">
+                  <a @click.prevent="nextStep(3, $t('teams.confirm_create_new_team'))">
+                    {{ $t('teams.create_new_team') }}
+                  </a>
+                </p>
+              </v-col>
+            </v-row>
+          </v-stepper-content>
 
-                  <v-row>
-                    <label class="col-md-5 col-form-label text-md-right">{{ $t('teams.team_id') }}</label>
-                    <label class="col-md-7 col-form-label-plain text-md-left">
-                      {{ teamTemp.code }}
-                    </label>
-                  </v-row>
+          <!-- STEP 2: VERIFY TEAM DETAILS (IF JOINING EXISTING TEAM)-->
+          <v-stepper-content step="2">
+            <v-row>
+              <v-col cols=12 class="mt-5 text-center">
+                <h3>{{ $t('teams.confirm_team_details') }}</h3>
+                <p class="text-muted my-8">
+                  {{ $t('teams.confirm_team_message') }}
+                </p>
 
-                  <v-row>
-                    <label class="col-md-5 col-form-label text-md-right">{{ $t('teams.team_name') }}</label>
-                    <label class="col-md-7 col-form-label-plain text-md-left">
-                      {{ teamTemp.display_name }}
-                    </label>
-                  </v-row>
+                <v-row>
+                  <label class="col-md-5 col-form-label text-md-right">{{ $t('teams.team_id') }}</label>
+                  <label class="col-md-7 col-form-label-plain text-md-left">
+                    {{ teamTemp.code }}
+                  </label>
+                </v-row>
 
-                  <v-row>
-                    <label class="col-md-5 col-form-label text-md-right">{{ $t('teams.contact_name') }}</label>
-                    <label class="col-md-7 col-form-label-plain text-md-left">
-                      {{ teamcontact.name }}
-                    </label>
-                  </v-row>
+                <v-row>
+                  <label class="col-md-5 col-form-label text-md-right">{{ $t('teams.team_name') }}</label>
+                  <label class="col-md-7 col-form-label-plain text-md-left">
+                    {{ teamTemp.display_name }}
+                  </label>
+                </v-row>
 
+                <v-row>
+                  <label class="col-md-5 col-form-label text-md-right">{{ $t('teams.contact_name') }}</label>
+                  <label class="col-md-7 col-form-label-plain text-md-left">
+                    {{ teamcontact.name }}
+                  </label>
+                </v-row>
+
+                <!--
                   <v-row>
                     <label class="col-md-5 col-form-label text-md-right">{{ $t('teams.contact_email') }}</label>
                     <label class="col-md-7 col-form-label-plain text-md-left">
                       {{ teamcontact.email }}
                     </label>
                   </v-row>
+                  -->
 
-                  <br><br>
-                  
-                  <v-btn id="cancel" @click.prevent="clearForm">
-                    {{ $t('general.go_back') }}
-                  </v-btn>
+                <br><br>
 
-                  <v-btn color="secondary" @click.prevent="joinTeam">
-                    {{ $t('teams.join_team') }}
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-stepper-content>
+                <v-btn id="cancel" @click.prevent="clearForm">
+                  {{ $t('general.go_back') }}
+                </v-btn>
 
+                <v-btn color="primary" @click.prevent="joinTeam">
+                  {{ $t('teams.join_team') }}
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-stepper-content>
 
-            <!-- STEP 3: ENTER NEW TEAM NAME (IF CREATING NEW TEAM)-->
-            <v-stepper-content step="3">
-              <v-row>
-                <v-col cols=12 class="mt-5 text-center">
-                  <h3 class="ma-3">{{ $t('teams.new_team_name') }}</h3>
-                  <blockquote class="blockquote text-muted">
-                    {{ $t('teams.new_team_name_help') }}
-                  </blockquote>
+          <!-- STEP 3: ENTER NEW TEAM NAME (IF CREATING NEW TEAM)-->
+          <v-stepper-content step="3">
+            <v-row>
+              <v-col cols=12 class="mt-5 text-center">
+                <h3 class="ma-3">{{ $t('teams.new_team_name') }}</h3>
+                <p class="text-muted my-8">
+                  {{ $t('teams.new_team_name_help') }}
+                </p>
 
-                  <v-text-field v-model="team_name" class="ma-3" :label="$t('teams.team_name')" outlined :error="teamNameError" :error-messages="teamNameErrorMsg"></v-text-field>
+                <v-text-field v-model="team_name" class="ma-3" :label="$t('teams.team_name')" outlined :error="teamNameError" :error-messages="teamNameErrorMsg"></v-text-field>
 
-                  <v-btn id="cancel" @click.prevent="clearForm">
-                    {{ $t('general.go_back') }}
-                  </v-btn>
-                  <v-btn color="secondary" @click.prevent="createTeam">
-                    {{ $t('general.create') }}
-                  </v-btn>
-                </v-col>
-              </v-row>
-            </v-stepper-content>
+                <v-btn id="cancel" @click.prevent="clearForm">
+                  {{ $t('general.go_back') }}
+                </v-btn>
+                <v-btn color="primary" @click.prevent="createTeam">
+                  {{ $t('general.create') }}
+                </v-btn>
+              </v-col>
+            </v-row>
+          </v-stepper-content>
 
-            
-            <!-- STEP 4: SUCCESS CONFIRMATION-->
-            <v-stepper-content step="4">
-              <v-row>
-                <v-col cols=12 class="mt-5 text-center">
-                  <v-icon size="100" color="success">mdi-check-circle-outline</v-icon>
-                  <p class="ma-10"><span class="display-2">{{ successMsg }}</span></p>
-                </v-col>
-              </v-row>
-            </v-stepper-content>
-          </v-stepper-items>
-        </v-stepper>
-    </v-card>
-  </v-container>
+          <!-- STEP 4: SUCCESS CONFIRMATION-->
+          <v-stepper-content step="4">
+            <v-row>
+              <v-col cols=12 class="mt-5 text-center">
+                <h3 class="ma-3">
+                  <v-icon size="30" color="success">mdi-check-circle-outline</v-icon>
+                  {{ this.$t('teams.welcome_to_team') }}
+                </h3>
+
+                <v-divider class="my-16" />
+
+                <div class="text-muted my-8">
+                  <!-- Tasks for team administrator -->
+                  <div v-if="isNewTeam">
+                    Notifications can now be enabled for your team.
+                    This allows team members
+                    Would you like to setup notifications for your new team?
+                  </div>
+
+                  <!-- Tasks for new team member -->
+                  <div v-if="!isNewTeam">
+                    View available schedules
+                    Subscribe to notifications for this team
+                  </div>
+                </div>
+              </v-col>
+            </v-row>
+          </v-stepper-content>
+
+          <!-- STEP 5: NOTIFICATIONS -->
+
+        </v-stepper-items>
+      </v-stepper>
+    </v-card-text>
+  </v-card>
+</v-container>
 </template>
 
 <script>
@@ -138,7 +162,7 @@ export default {
   layout: 'vuetify',
   mixins: [helper],
 
-  data () {
+  data() {
     return {
       userid: this.$userId,
       teamid: '',
@@ -148,19 +172,18 @@ export default {
       teamNameError: false,
       teamNameErrorMsg: '',
       teamFound: false,
-      successMsg: this.$t('teams.welcome_to_team'),
       teamTemp: [],
       teamcontact: [],
-      stepperCurr: 1,
+      stepperCurr: 4,
       stepperMax: 4,
+      isNewTeam: false,
     }
   },
 
-  created () {
-  },
+  created() {},
 
   methods: {
-    async nextStep (n, msg) {
+    async nextStep(n, msg) {
       var isNextStep = true
       console.log(msg)
 
@@ -175,7 +198,7 @@ export default {
       }
     },
 
-    async findTeam () {
+    async findTeam() {
       this.teamNotFound = false
       this.teamFound = false
 
@@ -198,17 +221,18 @@ export default {
       }
     },
 
-    joinTeam () {
+    joinTeam() {
       const formData = new FormData()
       formData.append('user_id', this.userid)
       formData.append('team_id', this.teamid)
       axios.post('/api/teams/jointeam', formData)
         .then(response => {
           this.stepperCurr = 4
+          this.isNewTeam = false
         })
     },
 
-    createTeam () {
+    createTeam() {
       this.hasError = false
 
       const formData = new FormData()
@@ -224,12 +248,12 @@ export default {
             this.setTeam(response.data.team)
 
             this.stepperCurr = 4
+            this.isNewTeam = true
           }
         })
     },
 
-
-    clearForm () {
+    clearForm() {
       this.teamNotFound = false
       this.teamNotFoundMsg = ''
       this.teamNameError = false
@@ -239,6 +263,7 @@ export default {
       this.teamcontact = []
       this.teamid = ''
       this.stepperCurr = 1
+      this.isNewTeam = false
     }
   }
 }
