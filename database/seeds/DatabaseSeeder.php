5e637b3ad6ea1a84cd0ebc37b90f24adca7f3b4e
Mike Jenkins <mjenkins.jw@gmail.com>
2020-12-01 13:02:05 +0100

    Database: fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //$this->call(RoleSeeder::class);
-        //$this->call(LanguageSeeder::class);
+        $this->call(RoleSeeder::class);
+        $this->call(LanguageSeeder::class);
 
         // Create an admin user
         $user = User::create([
